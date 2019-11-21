44b0a1317fc7981f5eedff181fa5d622b0f364bf
iruslanav <iruslanav@gmail.com>
2019-11-21 11:53:58 +0100

    archivos primera subida
    subir archivo

---
+++ Assets/Scripts/Player1.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : MonoBehaviour
+{
+    public Vector2 movementDirection;
+    public float movementSpeed;
+    public float MOVEMENT_BASE_SPEED=5.0f;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        processInput();
+        Move();
+        Animate();
+    }
+    void processInput()
+    {
+        movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        movementSpeed = Mathf.Clamp(movementDirection.magnitude, 0.0f, 1.0f);
+        movementDirection.Normalize();
+    }
+    void Move()
+    {
+        rb.velocity = movementDirection * movementSpeed * MOVEMENT_BASE_SPEED;
+    }
+    void Animate()
+    {
+        if (movementDirection != Vector2.zero)
+        {
+            animator.SetFloat("Horizontal", movementDirection.x);
+            animator.SetFloat("Vertical", movementDirection.y);
+        }
+        animator.SetFloat("Speed", movementSpeed);
+    }
+}
